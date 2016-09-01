73ec55311b907abadb7004ad0a326a85094feef8
Alex Howington <alex.howington@gmail.com>
2016-09-01 04:42:40 -0500

    Only allow one meal per day

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,5 +1,5 @@
 class Meal < ApplicationRecord
   validates :food, presence: true
-  validates :date_of, presence: true
+  validates :date_of, presence: true, uniqueness: true
   scope :recent, -> { order(date_of: :desc) }
 end
